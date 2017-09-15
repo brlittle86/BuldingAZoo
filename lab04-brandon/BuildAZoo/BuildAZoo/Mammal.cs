86df67fa27e698ce07422cebc21ae1c3e8da1b74
Brandon Little <brlittle86@gmail.com>
2017-09-15 09:42:40 -0700

    abstracted out the mammal class

--- lab04-brandon/BuildAZoo/BuildAZoo/Mammal.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Mammal.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace BuildAZoo
 {
-    class Mammal : Animal
+    abstract class Mammal : Animal
     {
+        public void HasFur()
+        {
+            Console.WriteLine("This animal has fur!");
+        }
+
+        public void LiveBirth()
+        {
+            Console.WriteLine("This animal gives live birth.");
+        }
+
+        public void HasBinocularVision()
+        {
+            Console.WriteLine("This animal has binocular vision");
+        }
     }
 }
