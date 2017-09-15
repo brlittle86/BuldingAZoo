975fbbf5c0921e523c41a84ec24b65eb0fd70e97
Brandon Little <brlittle86@gmail.com>
2017-09-15 09:08:51 -0700

    created base files for each class, began filling out Animal class

---
+++ lab04-brandon/BuildAZoo/BuildAZoo/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo
+{
+    abstract class Animal
+    {
+        public string Species { get; set; }
+        public string Name { get; set; }
+
+        public abstract void SleepHours();
+
+    }
+}
