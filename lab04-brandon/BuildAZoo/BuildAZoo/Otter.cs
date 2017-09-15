d39ef1166d4254d07eac678903908a67e6f7a6f0
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:05:24 -0700

    built out the Otter class and implemented required methods

--- lab04-brandon/BuildAZoo/BuildAZoo/Otter.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Otter.cs
@@ -6,5 +6,24 @@ namespace BuildAZoo
 {
     class Otter : Mammal
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("Otters tend to be nocturnal, but may be diurnal.");
+        }
+
+        public void IsAmphibious()
+        {
+            Console.WriteLine("Otters spend most of their lives in water, but are more than capable of living on land.");
+        }
+
+        public void IsIntelligent()
+        {
+            Console.WriteLine("Otters display high level puzzle solving intelligence and communication skills.");
+        }
+
+        public void FloatAndSleep()
+        {
+            Console.WriteLine("Otters often sleep while floating on their backs in the water.");
+        }
     }
 }
