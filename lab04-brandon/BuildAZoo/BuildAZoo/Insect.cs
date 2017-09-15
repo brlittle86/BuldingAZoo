2e5c2099761015f669603738723750b7754d0b22
Brandon Little <brlittle86@gmail.com>
2017-09-15 09:56:16 -0700

    abstracted the Insect class and gave it basic methods

--- lab04-brandon/BuildAZoo/BuildAZoo/Insect.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Insect.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace BuildAZoo
 {
-    class Insect : Animal
+    abstract class Insect : Animal
     {
+        public void HasCompoundEyes()
+        {
+            Console.WriteLine("This animal has compound eyes.");
+        }
+
+        public void HasManyLegs()
+        {
+            Console.WriteLine("This animal has more than four legs!");
+        }
+
+        public void BeenAround()
+        {
+            Console.WriteLine("This animal type has been around for hundreds of millions of years.");
+        }
     }
 }
