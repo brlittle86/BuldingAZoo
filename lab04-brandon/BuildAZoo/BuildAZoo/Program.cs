337c0c5ff699956e98e874ed35f12856556422b0
Brandon Little <brlittle86@gmail.com>
2017-09-15 11:25:48 -0700

    additional formatting for displaying information

--- lab04-brandon/BuildAZoo/BuildAZoo/Program.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Program.cs
@@ -6,20 +6,27 @@ namespace BuildAZoo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Let's learn about Akasha the Tiger!");
             Tiger akasha = new Tiger("Akasha");
             akasha.DoesWalk();
             akasha.HasFur();
             akasha.IsABigCat();
+            Console.WriteLine();
 
+            Console.WriteLine("Now, let's talk about Steve the Chameleon!");
             Chameleon steve = new Chameleon("Steve");
             steve.DoesWalk();
             steve.HasATail();
             steve.EatsInsects();
+            Console.WriteLine();
 
+            Console.WriteLine("Last, but not least, let's meet Miyuki the Praying Mantis!");
             PrayingMantis miyuki = new PrayingMantis("Miyuki");
             miyuki.DoesBreath();
             miyuki.HasCompoundEyes();
             miyuki.DeadlyFemale();
+
+            Console.Read();
         }
     }
 }
