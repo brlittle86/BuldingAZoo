ab0c8722a6206e529b985840df2440ba4fb3d691
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:25:00 -0700

    built out the chameleon class

--- lab04-brandon/BuildAZoo/BuildAZoo/Chameleon.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Chameleon.cs
@@ -6,5 +6,24 @@ namespace BuildAZoo
 {
     class Chameleon : Reptile
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("The Chameleon is generally diurnal.");
+        }
+
+        public void EatsInsects()
+        {
+            Console.WriteLine("The Chameleon eats small index by catching them with their fast, whip-like, sticky tongue.");
+        }
+
+        public void StealthyReptile()
+        {
+            Console.WriteLine("The Chameleon is able to change its skin color at will to provide extra stealth.");
+        }
+
+        public void TwoThumbs()
+        {
+            Console.WriteLine("The Chameleon has two opposing toes on each foot, so it's kind of like having two thumbs and three fingers.");
+        }
     }
 }
