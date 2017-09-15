b86dd0c97cae21b608096334d9f99c258a47c249
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:32:08 -0700

    built out the stag beetle class

--- lab04-brandon/BuildAZoo/BuildAZoo/StagBeetle.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/StagBeetle.cs
@@ -6,5 +6,24 @@ namespace BuildAZoo
 {
     class StagBeetle : Insect
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("Stag Beetles are generally diurnal.");
+        }
+
+        public void HasHorns()
+        {
+            Console.WriteLine("The Stag Beetle is distinguished by its signature horns.");
+        }
+
+        public void BlackColor()
+        {
+            Console.WriteLine("The Stag Beetle generally looks black, but actually ranges from dark purples to bold blues.");
+        }
+
+        public void GreatForParties()
+        {
+            Console.WriteLine("No Stag party is complete without Stag Beetles.");
+        }
     }
 }
