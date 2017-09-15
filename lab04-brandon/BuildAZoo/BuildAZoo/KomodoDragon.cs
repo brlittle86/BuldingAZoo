aa8d6435cd7f03dc211b8340446fc17f2be98bf9
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:14:27 -0700

    built out the KomodoDragon class

--- lab04-brandon/BuildAZoo/BuildAZoo/KomodoDragon.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/KomodoDragon.cs
@@ -6,5 +6,24 @@ namespace BuildAZoo
 {
     class KomodoDragon : Reptile
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("Komodo Dragons are mostly diurnal.");
+        }
+
+        public void VenomIsSaliva()
+        {
+            Console.WriteLine("Komodo Dragon saliva is highly venomous and full of bacteria.");
+        }
+
+        public void LargeReptile()
+        {
+            Console.WriteLine("Komodo Dragons often exceed 10 feet in length.");
+        }
+
+        public void RemoteHome()
+        {
+            Console.WriteLine("The Komodo Dragon comes from a small set of islands in the southeast Pacific Ocean.");
+        }
     }
 }
