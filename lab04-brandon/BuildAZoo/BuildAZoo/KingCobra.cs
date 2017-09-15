702631002cfb46f280bf60bc8926984280a9b216
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:28:53 -0700

    built out king cobra class

--- lab04-brandon/BuildAZoo/BuildAZoo/KingCobra.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/KingCobra.cs
@@ -6,5 +6,24 @@ namespace BuildAZoo
 {
     class KingCobra : Reptile
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("King Cobra are generally diurnal.");
+        }
+
+        public void IntimidatingHood()
+        {
+            Console.WriteLine("The King Cobra's hood is meant to make it look bigger and scarier to predators.");
+        }
+
+        public void IsVenomous()
+        {
+            Console.WriteLine("The King Cobra has a potent venom it injects into prey via needle-like fangs.");
+        }
+
+        public void IsFast()
+        {
+            Console.WriteLine("Despite having no legs, the King Cobra can achieve speeds above 12mph.");
+        }
     }
 }
