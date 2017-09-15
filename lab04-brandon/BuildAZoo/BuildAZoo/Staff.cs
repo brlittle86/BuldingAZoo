2a57a96736a0dfb5b75349e7f99aa589e7f6827d
Brandon Little <brlittle86@gmail.com>
2017-09-15 09:27:05 -0700

    built out the Staff abstract class

--- lab04-brandon/BuildAZoo/BuildAZoo/Staff.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Staff.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace BuildAZoo
 {
-    class Staff
+    abstract class Staff
     {
+        public abstract string Name { get; set; }
+        public string Gender { get; set; }
+
+        public void IsFriendly()
+        {
+            Console.WriteLine("They better be!");
+        }
+
+        public void CanDrive()
+        {
+            Console.WriteLine($"{Name} can drive!");
+        }
+
+        public void WorkingHours()
+        {
+            Console.WriteLine($"{Name} works from 9am to 5pm.");
+        }
     }
 }
