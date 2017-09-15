92fb53a3be271c2e652dc74dc33ed538c1de255e
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:36:32 -0700

    built out tarantual section

--- lab04-brandon/BuildAZoo/BuildAZoo/Tarantula.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/Tarantula.cs
@@ -6,5 +6,19 @@ namespace BuildAZoo
 {
     class Tarantula : Insect
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("Tarantulas tend to be diurnal.");
+        }
+
+        public void Arachnid()
+        {
+            Console.WriteLine("The Tarantula has 8 legs and spins a web.");
+        }
+
+        public void AmbushHunter()
+        {
+            Console.WriteLine("The Tarantual is an ambush hunter.");
+        }
     }
 }
