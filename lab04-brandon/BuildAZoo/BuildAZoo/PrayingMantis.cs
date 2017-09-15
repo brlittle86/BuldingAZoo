03f2c35af3309c3cfd283669181ad08aee9b3a00
Brandon Little <brlittle86@gmail.com>
2017-09-15 10:47:04 -0700

    built out praying mantis class

--- lab04-brandon/BuildAZoo/BuildAZoo/PrayingMantis.cs
+++ lab04-brandon/BuildAZoo/BuildAZoo/PrayingMantis.cs
@@ -6,5 +6,24 @@ namespace BuildAZoo
 {
     class PrayingMantis : Insect
     {
+        public override void SleepHours()
+        {
+            Console.WriteLine("The Praying Mantis is primarily diurnal.");
+        }
+
+        public void DeadlyFemale()
+        {
+            Console.WriteLine("The female Praying Mantis kills and eats her mate after mating.");
+        }
+
+        public void FastAttack()
+        {
+            Console.WriteLine("The Praying Mantis attacks very quickly and has inspired martial arts.");
+        }
+
+        public void LooksReligious()
+        {
+            Console.WriteLine("It gets its name from how it appears to be praying due to the position of its forelimbs.");
+        }
     }
 }
